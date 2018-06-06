4f0dfbe3f341143236a4327c177114b81ae07dd0
Surbhi-Gupta28 <39941503+Surbhi-Gupta28@users.noreply.github.com>
2018-06-06 12:49:29 +0530

    Add files via upload

---
+++ ass2.py
@@ -0,0 +1,33 @@
+#assignment no 2
+#question no 1
+print("hello welcome to the acadview online python course " )
+#question no 2
+print("acad" + "view")
+#question no 3
+x=input("enter value of x: ")
+y=input("enter value of y: ")
+z=input("enter value of z: ")
+print (x)
+print (y)
+print (z)
+#question no 4
+print(' "let\'s get started " ')
+#question no 5
+s = "acadview"
+course = "python"
+fees = 5000
+# python 3 m print bina bracket ke nahi chalta
+print( '%s %s %d' %(s, course , fees ))
+# They are used for formatting strings. %s acts a placeholder for a string while %d acts as a placeholder for a number.
+#Their associated values are passed in via a tuple using the % operator.
+
+#name = 'marcog'
+#number = 42
+#print ('%s %d' % (name, number))
+
+#question no 6
+name = "tony stark"
+salary = 1000000
+print('%s %d' % (name, salary))
+
+ 
